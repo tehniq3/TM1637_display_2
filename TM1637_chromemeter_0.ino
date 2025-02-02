76116a26a3ffb8a8866fb8081e6e2d81589bbb6b
Nicu FLORICA (niq_ro) <nicu.florica@gmail.com>
2025-02-02 19:55:08 +0200

    Add files via upload

---
+++ TM1637_chromemeter_0.ino
@@ -0,0 +1,39 @@
+/*
+ * used https://github.com/avishorp/TM1637 librarry
+ * chrometer by Nicu FLORICA (niq_ro)
+ * v.0 - initial version (minutes : seconds)
+*/
+
+//#include <Arduino.h>
+#include <TM1637Display.h>  // https://github.com/avishorp/TM1637
+
+// Module connection pins (Digital Pins)
+#define CLK 2
+#define DIO 3
+
+// The amount of time (in milliseconds) between tests
+#define TEST_DELAY   500
+
+TM1637Display display(CLK, DIO);
+
+int k = 0;
+unsigned long timp0, timp;
+int h, m, s;
+
+
+void setup()
+{
+  display.setBrightness(2); // 0...15
+  timp0 = millis()/1000;
+}
+
+void loop()
+{ 
+      timp = millis()/1000 - timp0;
+      h = timp/3600;
+      m = (timp%3600)/60;
+      s = (timp%3600)%60;
+      display.showNumberDecEx(100*m+s, (0x80 >> k%2), true); 
+k++;
+delay(TEST_DELAY);
+}
